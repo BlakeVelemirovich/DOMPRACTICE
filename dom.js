ca6bc1fc4f588b52fdd9ba91683cbcbbaf7263ec
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-02 16:47:16 -0400

    Finished exercises

--- dom.js
+++ dom.js
@@ -15,3 +15,20 @@ purpleTitle.textContent = 'I\m a purple h3';
 purpleTitle.style.color = 'purple';
 
 body.appendChild(purpleTitle);
+
+const divContainer = document.querySelector('div');
+
+const div = document.createElement('div');
+div.style.cssText = 'border: black; background: pink;';
+
+const p = document.createElement('p');
+p.textContent = 'ME TOO!';
+div.appendChild(p);
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I\m in a div!';
+div.appendChild(h1);
+
+body.appendChild(div);
+
+
